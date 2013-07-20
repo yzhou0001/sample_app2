5be6dc889c872b5b63d2c7b2ab4110ae04ad725d
Ying Zhou <yzhou0001@yahoo.com>
2013-07-20 15:14:24 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp2::Application.routes.draw do
+  get "users/new"
+  
   root 'static_pages#home'
-
+  match '/signup', to: 'users#new', via: 'get'
   match '/explore', to: 'static_pages#explore', via: 'get'
   match '/build', to: 'static_pages#build', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
+ 
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
