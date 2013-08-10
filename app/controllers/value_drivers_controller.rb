a2c38d5e4d8bf37626f26fd63cfdade7bca51939
Ying Zhou <yzhou0001@yahoo.com>
2013-08-10 11:23:35 -0400

    added Edit to value driver

--- app/controllers/value_drivers_controller.rb
+++ app/controllers/value_drivers_controller.rb
@@ -5,7 +5,29 @@ class ValueDriversController < ApplicationController
   end
   
   def new
-   
+    @value_driver = ValueDriver.new
+  end
+  
+   def create
+    @value_driver = ValueDriver.new(value_driver_params)
+      if @value_driver.save
+       
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @value_driver
+    else
+      render 'new'
+    end
+  end
+  
+ def update
+   @value_driver = ValueDriver.find(params[:id])
+    if @value_driver.update_attributes(value_driver_params)
+      flash[:success] = "Profile updated"
+
+      redirect_to value_driver_path
+    else
+      render 'edit'
+    end
   end
   
   def edit
@@ -15,5 +37,12 @@ class ValueDriversController < ApplicationController
   def index
     @value_drivers = ValueDriver.paginate(page: params[:page])
   end
+
+private
+
+     
+    def value_driver_params
+      params.require(:value_driver).permit(:name, :text)
+    end
 end
 
